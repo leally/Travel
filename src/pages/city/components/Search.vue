0b7632d88601daa0a245e3314580171036e7fdef
lea <xdipeng@sina.com>
2019-03-24 01:21:57 +0800

    add city-search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音" />
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'CitySearch'
 }
 </script>
 
