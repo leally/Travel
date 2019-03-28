fe8081cb1814d3d593e2feab6d2e6b20010ee3e6
lea <xdipeng@sina.com>
2019-03-29 00:59:43 +0800

    detail-banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="banner">
+    <img
+      class="banner-img"
+      src="http://img1.qunarzz.com/sight/p0/1902/80/80d7d3ef350b7fffa3.water.jpg_600x330_e7a26e24.jpg"
+    />
+    <div class="banner-info">
+      <div class="banner-title">世界之窗景区</div>
+      <div class="banner-number">
+        <span class="iconfont">&#xe608;</span>
+        39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        margin-top .14rem
+        padding 0 .4rem
+        line-height .32rem
+        height .32rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .25rem
+        .banner-icon
+          font-size .24rem
+</style>
