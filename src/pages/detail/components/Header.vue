f94150db441c1cc7b94e443c3df925d269a1bbe9
lea <xdipeng@sina.com>
2019-04-03 00:27:32 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -32,7 +32,7 @@ export default {
   },
   methods: {
     handleScroll () {
-      const top = document.documentElement.scrollTop
+      const top = document.documentElement.scrollTop || document.body.scrollTop
       if (top > 60) {
         let opacity = top / 140
         opacity = opacity > 1 ? 1 : opacity
@@ -47,6 +47,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
